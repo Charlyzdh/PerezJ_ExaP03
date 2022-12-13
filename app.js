e17775e0b6463b2f84f0d08198014c6a32bf9c5b
Charlyzdh <j.carlos_hernandez95@hotmail.com>
2022-12-12 22:37:55 -0600

    main

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var User = require('./models/user').User
+var session = require('express-session')
+
+
 var app = express()
 
-
 app.use('/public', express.static('public'));
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: 'secret123',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
+    console.log(req.session.user_id)
     res.render('index');
 })
 
@@ -48,11 +54,11 @@ app.post("/users", function(req, res){
 app.post("/sessions", function(req, res){
     User.findOne({
         email: req.body.email, password : req.body.password
-    },function(err, docs){
+    },function(err, user){
         if(err){
             console.log(err);
         }
-        console.log(docs);
+        req.session.user_id = user._id;
         res.send('Hello world!');
     })
 })
